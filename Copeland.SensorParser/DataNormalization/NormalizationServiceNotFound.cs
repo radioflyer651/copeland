d7b6c7f143aa32ca218be995a0ef648ed51f87bd
Richard Olson <richard@richardolson.org>
2025-06-25 21:06:05 -0500

    Naming...

--- Copeland.SensorParser/DataNormalization/NormalizationServiceNotFound.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Copeland.SensorParser.DataNormalization
-{
-    public class NormalizationServiceNotFound:Exception
-    {
-        public NormalizationServiceNotFound(): base("Normalization service not found for data object.") { }
-    }
-}
---
+++ Copeland.SensorParser/DataNormalization/NormalizationServiceNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Copeland.SensorParser.DataNormalization
+{
+    public class NormalizationServiceNotFoundException:Exception
+    {
+        public NormalizationServiceNotFoundException(): base("Normalization service not found for data object.") { }
+    }
+}
